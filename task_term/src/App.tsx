0efe3d2b0c2e7d833accb41e6cddc8b067c19c5a
uj-kim <ninakyj@gmail.com>
2025-11-19 19:52:31 +0900

    index 기반 상태관리 -> id기반 상태관리로 변경

--- task_term/src/App.tsx
+++ task_term/src/App.tsx
@@ -1,26 +1,46 @@
 import { useState } from "react";
 
 export default function App() {
-  const [checkedList, setCheckedList] = useState<boolean[]>(
-    TERM_LIST.map((term) => term.defaultChecked)
-  );
-  const allChecked = checkedList.every((v) => v);
-
-  const handleToggleItem = (index: number) => {
-    setCheckedList((prev) => {
-      const newCheckedList = [...prev];
-      newCheckedList[index] = !newCheckedList[index];
-      return newCheckedList;
+  const [checkedList, setCheckedList] = useState<CheckedList>(() => {
+    // TERM_LIST.map((term) => term.defaultChecked)
+    // 초기값 ('필수' 체크되어 있는 값 기준으로 렌더링)
+    const firstValues: CheckedList = {};
+    TERM_LIST.forEach((term) => {
+      firstValues[term.id] = term.defaultChecked;
     });
+    return firstValues;
+  });
+  // const allChecked = checkedList.every((term) => checkedList[term.id]);
+  // 각 리스트에 대해 checkedList[term.id]가 전부 true인지 검사
+  const allChecked = TERM_LIST.every((term) => checkedList[term.id]);
+
+  const handleToggleItem = (id: number) => {
+    // 피드백 :index 기반으로 하면 추후 데이터 추가/수정시 체크박스 상태랑 실제 항목이 안 맞을 우려가 있음
+    // 해결 : index -> id기반으로 변경
+    // setCheckedList((prev) => {
+    // next -> newCheckedList 좀 더 시맨틱한 변수명 사용
+    // const newCheckedList = [...prev];
+    // newCheckedList = !newCheckedList[index];
+    // return newCheckedList;
+
+    setCheckedList((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
   };
 
   const handleToggleAll = () => {
     const newChecked = !allChecked;
-    setCheckedList(TERM_LIST.map(() => newChecked));
+    // setCheckedList(TERM_LIST.map(() => newChecked));
+    const update: CheckedList = {};
+    TERM_LIST.forEach((term) => {
+      update[term.id] = newChecked;
+    });
+    setCheckedList(update);
   };
 
-  const allRequiredChecked = TERM_LIST.every((term, index) =>
-    term.required ? checkedList[index] : true
+  const allRequiredChecked = TERM_LIST.every((term) =>
+    term.required ? checkedList[term.id] : true
   );
 
   return (
@@ -37,12 +57,12 @@ export default function App() {
         </label>
       </div>
       <ul>
-        {TERM_LIST.map((term, index) => (
+        {TERM_LIST.map((term) => (
           <ListItem
             key={term.id}
             term={term}
-            checked={checkedList[index]}
-            onToggle={() => handleToggleItem(index)}
+            checked={checkedList[term.id]}
+            onToggle={() => handleToggleItem(term.id)}
           />
         ))}
       </ul>
@@ -50,6 +70,8 @@ export default function App() {
     </div>
   );
 }
+
+type CheckedList = Record<number, boolean>;
 
 type ListItemProps = {
   term: Term;
